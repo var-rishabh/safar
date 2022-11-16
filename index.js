72da3ec4d181922c66d494a86bee90cacbc9b19d
Rishabh Varshney <right.rishabh@gmail.com>
2022-11-16 18:47:11 +0530

    landing page

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 const colors = require('colors');
 
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('./assets'));
-
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
 app.get("/", async (req, res) => {
-    try {
-        return res.status(200).json({
-            status: 'success',
-            message: 'APIs running successfully',
-            data: null
-        });
-    } catch (err) {
-        return res.status(401).json({
-            status: 'failure',
-            message: 'APIs not running successfully',
-            data: null
-        });
-    }
+    res.render("home");
 });
 
 app.listen(process.env.PORT, console.log(`Server running on port ${process.env.PORT}`.magenta));
