767b0883ee74a78fca8586735d30ff1d5848241d
Rohit Verma <rohitpawanverma9@gmail.com>
2022-11-17 14:54:26 +0530

    chat back

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const app = require('./app')
-const connectWithDb= require('./config/db')
+const connectWithDb = require('./config/db')
 require('dotenv').config()
 
 connectWithDb();
 
-app.listen(process.env.PORT,()=>{
+const httpServer = app.listen(process.env.PORT, () => {
     console.log(`Server is running at port:${process.env.PORT}`)
 })
+// chat by socket.io
+const http = require('http').Server(app);
+const { Server } = require("socket.io");
+const io = new Server(httpServer);
+io.on('connection', (socket) => {
+    console.log('socket io connected!');
+    socket.on('welcome', username => {
+        if (username == "SAFAR") {
+            socket.broadcast.emit('chat message', { message: "Welcome To SAFAR's  ChatBot! How can I help you ?", username: username });
+        }
+    });
+    socket.on("typing", (name) => {
+        socket.broadcast.emit("typing", name);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('send-chat-message', (message, username) => {
+        console.log(`${username} : ${message}`);
+        socket.broadcast.emit('chat message', { username: username, message: message });
+    })
+});
+
