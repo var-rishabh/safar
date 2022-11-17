b57896f55ac47c4cfe2d122b504c11058a9407e5
Rishabh Varshney <65810355+var-rishabh@users.noreply.github.com>
2022-11-17 17:00:04 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const connectWithDb = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL, {
+        await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
